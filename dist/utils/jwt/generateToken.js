6d11a6715f8ac95ab2c6ee35798df6ae8aa5ae70
Mdhuna r <rmidhuna4@gmail.com>
2024-10-18 14:58:52 +0530

    updating jwt cookie updating5

--- dist/utils/jwt/generateToken.js
+++ dist/utils/jwt/generateToken.js
@@ -16,7 +16,7 @@ const generateTokens = (res, payload) => {
     const cookieName = payload.userRole === 'admin' ? 'adminRefreshToken' : 'userRefreshToken';
     res.cookie(cookieName, refreshToken, {
         httpOnly: true,
-        sameSite: 'strict',
+        sameSite: 'none',
         maxAge: 30 * 24 * 60 * 60 * 1000,
         secure: false,
     });
